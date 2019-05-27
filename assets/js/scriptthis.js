09e588de4b88545a4fa8ae1820e1e8d4c813000d
Kitty McDonagh <kittymcdonagh@gmail.com>
2019-05-27 10:01:56 +0000

    turn specific boxes to red, if red turn black

--- assets/js/scriptthis.js
+++ assets/js/scriptthis.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
-    $(".box").on("click", function(){                              // click on any box
-        var classNames=$(this).attr("class").split(" ");           // this splits out the box's classs names [box,one], [box,two] or [box,three]
-        $("." + classNames[1]).css("background-color", "red");     // all boxes with that number will change to red
+    //    $(".box").on("click", function(){                              // click on any element with a "box" class
+    //        var classNames=$(this).attr("class").split(" ");           // this splits out the box's classs names [box,one], [box,two] or [box,three]
+    //        $("." + classNames[1]).css("background-color", "red");     // all boxes with that number will change to red
+    //    });
+
+
+    $(".box").on("click", function() {                                  // click on any element with a "box" class
+
+        var classNames = $(this).attr("class").split(" ");              // this splits out the box's classs names [box,one], [box,two] or [box,three]
+        var boxClass = classNames[0];                                   // = "box"
+        var className = classNames[1];                                  // = "one", "two", or "three", depending on which box clicked
+        if ($(this).css("background-color") == "rgb(255, 0, 0)") {      // if this object is already red, turn it black
+            $("." + className).css("background-color", "#000");
+        }
+        else {
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "rgb(255, 0, 0)");
+        }
     });
-     
-});
-
-
+})
