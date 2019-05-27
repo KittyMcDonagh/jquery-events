fe09c8bdaac2b606ac6411545df3344930466d7a
Kitty McDonagh <kittymcdonagh@gmail.com>
2019-05-27 08:19:59 +0000

    challenges 1-4 added

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,12 +85,85 @@ $(document).ready(function() {
 
     // jQuery effects - Challenge 1. Make the first button hide when clicked
 
-    $("#button-1").mouseenter(function() {
-        $("#button-1").fadeTo(1000, 0.5);
-    });
-    
-    // additional code - make button re-appear
-    $("#button-1").mouseleave(function() {
-        $("#button-1").fadeTo(1000, 1);
-    });
+    //  $("#button-1").mouseenter(function() {
+    //       $("#button-1").fadeTo(1000, 0.5);
 });
+
+// additional code - make button re-appear
+//   $("#button-1").mouseleave(function() {
+//     $("#button-1").fadeTo(1000, 1);
+//    });
+
+// $("#button-1").removeClass("bottom_button");
+// $("#button-1").removeClass("bottom_button").addClass("makeRed");
+// $("#button-1").slideUp(2000).slideDown(2000);
+// $("img").attr("src", "assets/images/kitty.png");                  // select attribute of an element and replace it
+
+// Method Chaining - Challenge 1
+
+// Create class to turn button red and add to html. Create border class, dont add to html. 
+// Remove red class, add border on mouseenter, vice versa on mouseleave.
+
+$("#button-1").mouseenter(function() {
+    $(this).removeClass("makeRed").addClass("makeBorder");     // 'this' instead of repeating "button-1"
+});
+
+$("#button-1").mouseleave(function() {
+    $(this).removeClass("makeBorder").addClass("makeRed");      // 'this' instead of repeating "button-1"
+});
+
+// Method Chaining - Challenge 2
+
+// Slide paragraph up / down
+// $("#button-1").click(function() {
+//    $("#par-1").slideToggle(2000);
+// });
+
+// $("#button-1").click(function() {
+//    $("#par-1").slideDown(2000);
+// });
+
+// hide / show paragraph 
+	$("#button-1").click(function() {
+		$("#par-1").hide(2000).show(2000);
+	});
+
+// Method Chaining - Challenge 3
+
+// Mouseover: fade in / out on specific button. This affects only the paragraph selected
+
+//	$("#button-2").mouseover(function() {
+//		$("#par-2").fadeOut(2000).fadeIn(2000);
+//	});
+	
+// Mouseover: show / hide on specific button. This affects only the paragraph selected
+
+//	$("#button-3").mouseover(function() {
+//		$("#par-3").hide(2000).show(2000);
+//	});
+	
+// Mouseover: fade in / out on all buttons. All buttons will react when you mouseover one.
+
+	$("button").mouseover(function() {
+		$("p").fadeOut(2000).fadeIn(2000);
+	});
+	
+// Mouseover: show / hide on all buttons. All buttons will react when you mouseover one.
+
+	$("button").mouseover(function() {
+		$("p").hide(2000).show(2000);
+	});
+
+// Method Chaining - Challenge 4
+
+//Go to eir.ie and using developer tools, change all the links so that they point to codeinsitute.net
+
+$("a").attr("href","https://codeinstitute.net/"); // Note that if the "https://" is not included, it will tag "codeinstitute.net" onto eir.ie.
+    
+
+
+
+
+
+
+
